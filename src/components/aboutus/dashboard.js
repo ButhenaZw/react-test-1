cb581c5332aac79ec5b66307a2ace78e5c8bc16f
ButhenaZw <bouthena762002@gmail.com>
2025-02-01 14:07:14 +0300

    commit

---
+++ src/components/aboutus/dashboard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import History from "./history/history"
+import Vision from "./Vision/vision"
+import Mission from "./mission/mission"
+import Hero from './Hero/hero'
+
+function App() {
+  return (
+<>
+<Hero/>
+<Vision/>
+<Mission/>
+
+<History/>
+
+
+
+
+</>
+  );
+}
+
+export default App;
